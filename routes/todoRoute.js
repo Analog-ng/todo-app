86abe1d2add18fcddb81a55b9703714a9cb3a4b9
Oluwabamise Olatunji <tunjibamise94@gmail.com>
2022-11-12 08:28:50 +0100

    Initial commit

--- routes/todoRoute.js
+++ routes/todoRoute.js
@@ -1,1 +1,19 @@
+const express = require('express');
+
+const router = express.Router();
+const controller = require('../controllers/flightController');
+
+
+router
+    .get('/', controller.getAllFlights)
+    .get('/:id', controller.getOneFlight)
+    .post('/', controller.createFlight)
+    .put('/:id', controller.updateFlight)
+    .delete('/:id', controller.deleteFlight);
+
+
+    
+    
+
+module.exports = router;
 
